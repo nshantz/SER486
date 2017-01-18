2659829f16203bad227a5c6fc7dfb913475f6603
nshantz <nshantz@asu.edu>
2017-01-18 02:33:46 +0000

    Adding more in depth comments

--- assignments/assign2/8leds.c
+++ assignments/assign2/8leds.c
@@ -3,7 +3,7 @@
 
 //maximum number of lights available
 #define LIGHT_NUM 8
-
+//LED Light pin numbers
 #define GPIO1 0
 #define GPIO2 2
 #define GPIO3 3
@@ -12,9 +12,9 @@
 #define GPIO6 5
 #define GPIO7 4
 #define GPIO8 1
-
-#define BUT1 10
-
+//button pin location
+#define BUT1 7
+//function to toggle light on and off
 int toggle(int status);
 
 int main(void)
@@ -26,6 +26,7 @@ int main(void)
 					GPIO4, 0, GPIO5, 0, GPIO6, 0, 
 					GPIO7, 0, GPIO8, 0
 					};
+	//initializing all lights to off				
 	int i = 0;
 	printf("Configuring Pins\n");
 	while(i < LIGHT_NUM * 2)
@@ -40,26 +41,34 @@ int main(void)
 	printf("Toggling Pins\n");
 	for(;;)
 	{
+		//toggle status value for LED
 		array[j+1] = toggle(array[j+1]);
+		//if 1, turn LED high
 		if(array[j+1] == 1)
 		{
 			digitalWrite(array[j], HIGH); delay(500);
 		}
+		//if 0, turn LED low
 		else
 		{
 			digitalWrite(array[j], LOW); delay(500);
 		}
 
+		//skipping by 2 to access the next pin and not the status pin
 		j = j + 2;
+		//since there are two values per pin, the max number must be multiplied by 2
+		//if number is twice the max, then the loop starts back at 0
 		if(j == LIGHT_NUM * 2)
 			j = 0;
+		//if number is below 0, then the loop starts back at the tail of the loop
 		else if(j == -1)
 			j = (LIGHT_NUM * 2) - 1;
 	}
 
 	return 1;
 }
-
+//insert the status value to read in
+//this section inverts status value
 int toggle(int status)
 {
 	if(status == 1)
